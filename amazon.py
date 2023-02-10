f7bc04a6067d8c16dd88c63e57cce66ab3b465a9
DENNIS DAMIEN <98984101+dennisdamien@users.noreply.github.com>
2023-02-10 20:41:35 +0530

    Update amazon.py

--- amazon.py
+++ amazon.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import json
+import time
 
 
 browser=webdriver.Chrome()
